040de124238689af828ea8d0c28ace2cb5f0b10d
HenryVC08 <henryvaldezcusi.08@gmail.com>
2025-07-16 17:08:30 -0500

    agregando useMemo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 
-
+import { useMemo } from "react"
 export default function Header({ cart }) {
 
     //state DERIVADO
-    const isEmpty = () => cart.length == 0
-    const cartTotal = () => cart.reduce((total,item ) => total +(item.quantity * item.price), 0)
+    const isEmpty = useMemo(()=> cart.length == 0, [cart]) 
+    const cartTotal = useMemo(() => cart.reduce((total,item ) => total +(item.quantity * item.price), 0), [cart])
 
     return (
         <header className="py-5 header">
@@ -23,7 +23,7 @@ export default function Header({ cart }) {
 
                             <div id="carrito" className="bg-white p-3">
 
-                                {isEmpty() ? (
+                                {isEmpty ? (
                                     <p className="text-center">El carrito esta vacio</p>
                                 ) : (
 
@@ -89,7 +89,7 @@ export default function Header({ cart }) {
                                             </tbody>
                                         </table>
 
-                                        <p className="text-end">Total pagar: <span className="fw-bold">{cartTotal()}</span></p>
+                                        <p className="text-end">Total pagar: <span className="fw-bold">{cartTotal}</span></p>
                                     </>
 
                                 )}
